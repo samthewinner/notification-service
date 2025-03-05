d795896b47a7c0fdd907814df8caeb170580271e
Abhijeetsingh Meena <abhijeet040403@gmail.com>
2025-03-05 22:25:56 +0530

    refactor: improve frontend/start.sh script

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# TODO: currently the script removes existing containers and runs new ones
+# TODO: find a better way if there exists one
 docker rm -f frontend-service-container
-docker run -it -p 8000:8000 --name frontend-service-container frontend-service
+docker run -p 8000:8000 --network notification-service --name frontend-service-container frontend-service
